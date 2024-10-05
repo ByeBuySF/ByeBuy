8d7e15f248134be8b4adc6d44291c8f43fd24401
Darren Sapalo <darren.sapalo@gmail.com>
2024-10-05 15:03:31 -0700

    Simple webserver with flask

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+# Simple Python web server 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
